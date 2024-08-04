794ab153810f8c86d36f1aac15136a0690e63a1d
MuhammadShakirdev <chaudhryshakir67@gmail.com>
2024-08-04 23:42:38 +0500

    Arrays Basics 01

---
+++ JavaScript-Arrays/arrays-basics.js
@@ -0,0 +1,11 @@
+let arry = [1, 2, 3, 4, 5, 6];
+let arry1 = ["Shakir", "Saad", "Raheel", "Sami`"];
+let arry2 = ["Shakir", true, 1];
+
+console.log(arry);
+console.log(arry1);
+console.log(arry2);
+
+// another way of declearing arrays
+let newArray = new Array(1, 2, 3, 4, 5);
+console.log(newArray);
